d7c1385afa2358cc4afb85a86759a9d480de88c0
Himanshu Kumar <96580706+Himanshu2239@users.noreply.github.com>
2025-05-06 10:11:55 +0530

    Update version.js

--- api/version.js
+++ api/version.js
@@ -1,8 +1,10 @@
-// Simple version endpoint
-module.exports = (req, res) => {
+// File: api/version.js
+// Simple version endpoint (ES Module)
+
+export default function handler(req, res) {
   res.status(200).json({
     name: 'CS Assessment System',
     version: '1.0.0',
     api: true
   });
-};
+}
